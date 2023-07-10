78d25ce0b61cad576e8fc71d77257d979097af5c
RENUKA KRISHNA <renukakrishna@Renuka-Krishna-MacBook-Pro.local>
2023-07-10 17:41:09 +0530

    push

--- javaScript/Display-entered-element.js
+++ javaScript/Display-entered-element.js
@@ -11,3 +11,61 @@ Addlist.addEventListener('click',function(){
     list_1.appendChild(newListElement);
 });
 
+
+// function getTodollist(){
+//     var http = new XMLHttpRequest();
+//     http.open('GET',"https://jsonplaceholder.typicode.com/",true);
+//     http.send();
+// }
+// getTodollist()
+
+
+var obj=[{
+    "id" : 25,
+    "name" : "Renu",
+    "age" : 22}
+,
+{
+    "id" : 24,
+    "name" : "Renuka",
+    "age" : 23
+}]
+fetch('https://jsonplaceholder.typicode.com/users',
+    {
+        method:"POST",
+        headers:{
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+
+
+// fetch('https://jsonplaceholder.typicode.com/users/1',
+//     {
+//         method:"PUT",
+//         headers:{
+//         'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(obj)
+//     })
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users/1',
+//     {
+//         method:"DELETE",
+//         headers:{
+//         'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(obj)
+//     })
+
+
+fetch('https://jsonplaceholder.typicode.com/users/1',
+    {
+        method:"GET",
+        headers:{
+        'Content-Type': 'application/json'
+        },
+    })
+
