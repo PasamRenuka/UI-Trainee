e11f5e6a2bd9d6e15975debe396d014ca8019e85
RENUKA KRISHNA <renukakrishna@Renuka-Krishna-MacBook-Pro.local>
2023-07-21 15:44:50 +0530

    push

--- Reactjs/learning_from_youtube/src/App.js
+++ Reactjs/learning_from_youtube/src/App.js
@@ -1,63 +1,14 @@
 import React from "react";
 import Form from "./form.js"
 import "./App.css";
-// import { useState } from "react";
-
-
 
 const App = () => {
-//   const [message, setMessage] = useState('');
 
-//   const handleMessageChange = event => {
-//     setMessage(myArray);
-//   };
-//   const [myArray, setMyArray] = useState('');
-//   const changeHandler = (event) => {
-//     setMyArray({ myArray: [event.target.value] });
-//   };
-//   const clickHandler = (datum) => {
-//     setMyArray(myArray + datum);
-//   };
-//   const clickClear=event=>{
-//     setMessage("")
-//     setMyArray("")
-//   }
-//   const comments = ["working", "sleeping", "eating", "coding"];
-
-
-//   return (
-//     <div className="main2">
-//         <h1>Feed back Example</h1>
-
-//       <header className="App-header">
-//       {comments.map((datum, index) => {
-//       return <button className="comment-btn" onClick={() => clickHandler(datum)}>
-//           {datum}
-//         </button>;
-//       })}
-//       <br />
-//       <textarea
-//         rows="6"
-//         cols="30"
-//         value={myArray}
-//         onChange={(event) => changeHandler(event)}
-//         placeholder="type your comment here"
-        
-//       />
-//       <br></br>
-//       <button className="Sub" onClick={()=>handleMessageChange()}>Submit</button>
-//       <button className="clear" onClick={clickClear}>Clear</button>
-//       <br></br>
-//       <p>  Output :     {message}</p>
-//       </header>
-//     </div>
-//   );
-// };
-return(
-  <div className="main">
-    <Form/>
-  </div>
-)
+  return (
+    <div className="main">
+      <Form />
+    </div>
+  )
 }
 
 export default App;
--- Reactjs/learning_from_youtube/src/form.js
+++ Reactjs/learning_from_youtube/src/form.js
@@ -3,50 +3,37 @@ import './App.css';
 import Display from './Display';
 
 function Form() {
-
   const [name, setName] = useState('');
   const [loc, setloc] = useState('');
   const [rating, setrating] = useState('');
   const [text, settext] = useState('')
   const [btn, setbtn] = useState({})
-
-
-  const [veiwName, setViewName] = useState('');
-  const [veiwLoc, setViewLoc] = useState('');
-  const [ViewRate, setViewRate] = useState('');
-  const [veiwText, setViewText] = useState('');
   const [show, setshow] = useState(false)
   const [array, setarray] = useState('');
 
-
-
-
   const handleChange = (e) => {
     setName(e.target.value);
-    if (name !== "")
-      setbtn({
-        display: 'block'
-      });
+
+    setbtn({
+      display: 'block'
+    });
   }
 
   const handleSubmit = (e) => {
     setshow(true);
     e.preventDefault();
     setName("")
-
+    setloc("")
+    setrating("")
+    setbtn({
+      display: 'none'
+    })
+    settext("")
   }
 
-
-
   const handleMessageChange = event => {
-    setViewName(name);
-    setViewText(text);
-    setViewLoc(loc);
-    setViewRate(rating);
     setarray((message) => [...message, { id: message.length, Name: name, Location: loc, Review: text, Rating: rating }])
     settext("")
-
-
   };
   const options = ['Chennai', 'Hyderabad', 'Bengaluru', 'Vijayawada', 'Kerala'];
   const onOptionChangeHandler = (event) => {
@@ -56,8 +43,6 @@ function Form() {
   const onOptionChangeHandlerRating = (event) => {
     setrating(event.target.value)
   }
-
-
   const changeHandler = (event) => {
     settext(event.target.value);
   };
@@ -66,50 +51,44 @@ function Form() {
       <header className="App-header">
         <form onSubmit={(e) => { handleSubmit(e) }}>
           <h2> Sign-up Form </h2>
-          <label >
-            Name:
-          </label>
-          <input type="text" value={name} required onChange={(e) => { handleChange(e) }} /><br />
-          <center>
-            <label>Location:</label>
-
-            <select onChange={onOptionChangeHandler}>
-
-              <option>Please choose Location</option>
-              {options.map((option, index) => {
-                return <option key={index} >
-                  {option}
-                </option>
-              })}
-            </select>
-          </center>
-
-          <center>
-
-            <label>Rating:</label>
-            <select onChange={onOptionChangeHandlerRating}>
-
-              <option>Please give rating</option>
-              {options1.map((option, index) => {
-                return <option key={index} >
-                  {option}
-                </option>
-              })}
-            </select>
-          </center>
+          <div className='details'>
+            <label className='lab'>
+              Name:
+            </label>
+            <input className='name' type="text" value={name} required onChange={(e) => { handleChange(e) }} /><br />
+            <center>
+              <label >Location:</label>
+              <select onChange={onOptionChangeHandler} className='location' value={loc}>
+                <option>choose Location</option>
+                {options.map((option, index) => {
+                  return <option key={index} >
+                    {option}
+                  </option>
+                })}
+              </select>
+            </center>
+            <center>
+              <label className='lab'>Rating:</label>
+              <select onChange={onOptionChangeHandlerRating} value={rating} className='rating'>
+                <option>Please give rating</option>
+                {options1.map((option, index) => {
+                  return <option key={index} >
+                    {option}
+                  </option>
+                })}
+              </select>
+            </center>
+          </div>
           <textarea
             rows="6"
             cols="30"
+            value={text}
             onChange={(event) => changeHandler(event)}
-            placeholder="type your comment here"
-
-          /><br />
+            placeholder="type your comment here" /><br />
           <input type="submit" value="Submit" id="sub" style={btn} onClick={() => handleMessageChange()} />
         </form>
         {console.log(array)}
         {show && array.map((data, index) => <Display props={data} />)}
-
-
       </header>
     </div>
   );
