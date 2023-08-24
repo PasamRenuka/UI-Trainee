bedb33c4e25ea2dabca092f716b00a5bcbc39a4c
PasamRenuka <pasam@experience.com>
2023-08-24 16:27:27 +0530

    push

---
+++ WebPack/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Butterfly from 'icons/butterfly.js';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Webpack React Image App</h1>
+      <Butterfly/>
+    </div>
+  );
+}
+
+export default App;
